e4802feb4ce0b2463c360566366bbe06b0b7ceae
fanliwen <flw_dream@126.com>
2019-01-22 17:39:33 +0800

    update snapshot

--- src/markdown-it-plugin-flowchart.js
+++ src/markdown-it-plugin-flowchart.js
@@ -1,12 +1,11 @@
 export default (md) => {
-    const temp = md.renderer.rules.fence.bind(md.renderer.rules)
-    md.renderer.rules.fence = (tokens, idx, options, env, slf) => {
-      const token = tokens[idx]
-      if (token.info === 'flowchart.js') {
-        const code = token.content.trim()
-        return `<div class="md-flowchart">${code}</div>`
-      }
-      return temp(tokens, idx, options, env, slf)
+  const temp = md.renderer.rules.fence.bind(md.renderer.rules)
+  md.renderer.rules.fence = (tokens, idx, options, env, slf) => {
+    const token = tokens[idx]
+    if (token.info === 'flowchart.js') {
+      const code = token.content.trim()
+      return `<div class="md-flowchart">${code}</div>`
     }
+    return temp(tokens, idx, options, env, slf)
   }
-  
+}
